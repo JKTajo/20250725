36713ee163046048a5b313d38481334c21a5cc54
JKTajo <junyungi123@naver.com>
2025-07-31 14:52:52 +0900

    Update Seoulbike.py

--- Seoulbike.py
+++ Seoulbike.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyPO5FmTa0qqBgiZ7W9BwKaf",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
